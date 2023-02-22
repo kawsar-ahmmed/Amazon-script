2629cf70fb649756630954555906ce4efc36725a
kawsar-ahmmed <admin@Kawsars-MacBook-Pro.local>
2023-02-22 14:51:01 +0600

    Amazon-Script

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPost></LoadPost>
+      <Distric name= {'Noakhali'} special = {'Bivhag'} ></Distric>
+      <Distric name= {'Dhaka'} special = {'Lalbag Kolla'} ></Distric>
+      <Distric name= {'ASH'} special = {'School'} ></Distric>
     </div>
   );
+}
+
+
+//Load
+
+function LoadPost (){
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  }, [])
+
+  return (
+    <div className="newPost">
+      <h1>Post: {posts.length} </h1>
+      {
+        posts.map (post => <Post title={post.title} description={post.body} ></Post> )
+      }
+    </div>
+  )
+}
+//
+function Post (props){
+  return(
+    <div className="post">
+      <h2>Title: {props.title} </h2>
+      <p>Body:{props.description}</p>
+      
+    </div>
+  )
+}
+
+// load end
+function Distric (props) {
+  const [power, setPower] = useState(1);
+
+  const boosPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  } 
+  return (
+    <div className="distric">
+      <h2>Name: {props.name} </h2>
+      <h3>Special: {props.special} </h3>
+      <h4>Power: {power} </h4>
+      <button onClick={boosPower}> Boost The Power</button>
+    </div>
+  )
 }
 
 export default App;
